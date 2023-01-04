7302ea736a1f3a8ba697df0191e79227a27239e0
venus003 <nlstolp@hotmail.com>
2023-01-03 18:45:20 -0600

    gradePercent

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Grade Percentage: ");
+        String gradePercent = Console.ReadLine();
+
+        int.Parse(gradePercent);
+
+        Console.Write($"{gradePercent}");
     }
 }
