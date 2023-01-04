7c36b960fafa3d3a2bddffe085d6802b7562fee4
venus003 <nlstolp@hotmail.com>
2023-01-03 18:38:18 -0600

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
