48ee9c7d9539cd42155e7384a49a06561718a0de
venus003 <nlstolp@hotmail.com>
2023-01-07 18:59:20 -0600

    grade percents

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,10 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Grade Percentage: ");
-        String gradePercent = Console.ReadLine();
+        String input = Console.ReadLine();
 
-        int.Parse(gradePercent);
+        int gradePercent = int.Parse(input);
+        string letter = "F";
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
 
-        Console.Write($"{gradePercent}");
+        Console.WriteLine(letter);
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Seems you didn't pass. You'll do better next time!");
+        }
     }
 }
