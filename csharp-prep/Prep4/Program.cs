4e58477256f36a4be460f4d4f243a39c930bab22
venus003 <nlstolp@hotmail.com>
2023-01-09 12:48:31 -0600

    list stats

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int input = -1;
+        List<int> numbers = new List<int>();
+
+        while (input != 0)
+        {
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        int total = numbers.Sum();
+        float average = ((float)total / numbers.Count);
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
