0d5ec3fda3909619f7a88e5b41e95734c779772b
venus003 <nlstolp@hotmail.com>
2023-01-09 11:19:43 -0600

    number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (answer != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (answer == guess)
+            {
+                Console.Write("You guessed it!");
+            }
+            else if (answer < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (answer > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
