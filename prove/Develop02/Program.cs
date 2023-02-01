254756d676d96fb1dc82b39dc4094e272bd53260
venus003 <nlstolp@hotmail.com>
2023-02-01 11:32:56 -0600

    updates

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+        int input = 0;
+        Journal journal1 = new Journal();
+        
+        while (input != 5)
+        {
+            journal1._menu = @"Please select one of the following choices:
+1. Write
+2. Display
+3. Load
+4. Save
+5. Quit
+";
+            journal1.Display();
+            Console.Write("What would you like to do? ");
+            input = int.Parse(Console.ReadLine());
+            if (input == 1)
+            {
+                journal1.Write();
+            }
+            else if (input == 2)
+            {
+                journal1.EntriesDisplay();
+            }
+            else if (input == 3)
+            {
+                journal1.Load();
+            }
+            else if (input == 4)
+            {
+                journal1.Save();
+            }
+        }
     }
 }
