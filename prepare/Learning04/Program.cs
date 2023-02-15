4704f0141768e7eeb54d8c8cdb1e53eaaae89a5e
venus003 <nlstolp@hotmail.com>
2023-02-15 10:26:57 -0600

    inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Beholder", "Anti-magic");
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment mathProbs = new MathAssignment("Nathan", "Coding", "7.3", "8-19");
+        Console.WriteLine(mathProbs.GetSummary());
+        Console.WriteLine(mathProbs.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Nathan", "English", "The Rise of Video Games");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
