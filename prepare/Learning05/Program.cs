3e40031d7fd642ba37c111190b533857a81fcf42
venus003 <nlstolp@hotmail.com>
2023-03-01 18:00:17 -0600

    polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 2);
+        Rectangle s2 = new Rectangle("green", 2, 3);
+        Circle s3 = new Circle("red", 2);
+
+        shapes.Add(s1);
+        shapes.Add(s2);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"{color} {area}");
+        }
     }
 }
